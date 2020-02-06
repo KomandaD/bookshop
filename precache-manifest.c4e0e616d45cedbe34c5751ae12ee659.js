c28b8ab4e8b2d9f56166355c1fc47bcd3b39cee8
KomandaD <54846912+KomandaD@users.noreply.github.com>
2020-02-07 00:08:05 +0300

    Add files via upload

---
+++ precache-manifest.c4e0e616d45cedbe34c5751ae12ee659.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8e070282756aa1f7f4ef6b35262919",
+    "url": "/bookshop/index.html"
+  },
+  {
+    "revision": "ad006c0e0e5e7c10b1ba",
+    "url": "/bookshop/static/css/main.e3374fa6.chunk.css"
+  },
+  {
+    "revision": "2631dde65c5c3ed12fc1",
+    "url": "/bookshop/static/js/2.011c3f64.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bookshop/static/js/2.011c3f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad006c0e0e5e7c10b1ba",
+    "url": "/bookshop/static/js/main.06cba7d8.chunk.js"
+  },
+  {
+    "revision": "64437903d18b21107207",
+    "url": "/bookshop/static/js/runtime-main.4c7e40e8.js"
+  },
+  {
+    "revision": "2cd2557cec32a2422266366ed828d24c",
+    "url": "/bookshop/static/media/1.2cd2557c.jpg"
+  },
+  {
+    "revision": "a35aa0357bb13f20ec76b9de47285bf1",
+    "url": "/bookshop/static/media/10.a35aa035.jpg"
+  },
+  {
+    "revision": "8d779ad53339830bfa51746e01da88b7",
+    "url": "/bookshop/static/media/11.8d779ad5.jpg"
+  },
+  {
+    "revision": "d2b63f08e787e873d882d1e5e044f1f1",
+    "url": "/bookshop/static/media/12.d2b63f08.jpg"
+  },
+  {
+    "revision": "4e8874c76fd2081272e129126b88f3e2",
+    "url": "/bookshop/static/media/13.4e8874c7.png"
+  },
+  {
+    "revision": "db1312a9281d0b468fff09fd4a10b9e0",
+    "url": "/bookshop/static/media/14.db1312a9.jpg"
+  },
+  {
+    "revision": "6c17b8e122cb68446623c1869a45c5d5",
+    "url": "/bookshop/static/media/15.6c17b8e1.jpg"
+  },
+  {
+    "revision": "e2cc24d9158b52e00d080b8054988b08",
+    "url": "/bookshop/static/media/16.e2cc24d9.jpg"
+  },
+  {
+    "revision": "a8c151de939e884b2c6eef0a87aa2328",
+    "url": "/bookshop/static/media/17.a8c151de.jpg"
+  },
+  {
+    "revision": "5e3de3fd389f5712cb8a69f5fb8567fc",
+    "url": "/bookshop/static/media/2.5e3de3fd.jpg"
+  },
+  {
+    "revision": "ce9ff2d9ef67e389f2754d5673b2cfb3",
+    "url": "/bookshop/static/media/3.ce9ff2d9.jpg"
+  },
+  {
+    "revision": "f3f83a51d27aa2ebc87a4f261a64a096",
+    "url": "/bookshop/static/media/4.f3f83a51.jpg"
+  },
+  {
+    "revision": "6ee3f1705c7209fb530339423ab45543",
+    "url": "/bookshop/static/media/5.6ee3f170.jpg"
+  },
+  {
+    "revision": "6ba99e5db146e2775aa2824b487fbae7",
+    "url": "/bookshop/static/media/6.6ba99e5d.jpg"
+  },
+  {
+    "revision": "933fb532851978582d03848ea03fa496",
+    "url": "/bookshop/static/media/7.933fb532.jpg"
+  },
+  {
+    "revision": "d578758e4a94ce74afd0f3f14c01ca2f",
+    "url": "/bookshop/static/media/8.d578758e.jpg"
+  },
+  {
+    "revision": "99489a059bb92a8a39a54f6a69bd2480",
+    "url": "/bookshop/static/media/9.99489a05.jpg"
+  }
+]);
